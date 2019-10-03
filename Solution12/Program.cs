36a8416f1411fe03747b5a217e4a3f106ed63ec9
Simo Andreev <simeon.zlatanov.andreev@gmail.com>
2019-10-03 22:35:22 +0300

    [Initial-er Commit]

---
+++ Solution12/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Solution12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
