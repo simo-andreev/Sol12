894a8af091fbba73b81e03ea33270494dd3a991d
Simo Andreev <simeon.zlatanov.andreev@gmail.com>
2019-10-04 00:53:32 +0300

    misc: MISc - it moves!

--- Solution12/SoScene.cs
+++ Solution12/SoScene.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Globalization;
 using System.Numerics;
 using Adfectus.Common;
@@ -10,9 +12,19 @@ namespace Solution12
 {
     public class SoScene : Scene
     {
-        private Vector3 _position = new Vector3(50, 100, 0);
-        private readonly Vector2 _size = new Vector2(10, 10);
-        private const int MoveStep = 5;
+        private readonly Random _random = new Random();
+
+        private Vector3 _positionPlayer = new Vector3(50, 100, 0);
+        private readonly Vector2 _sizePlayer = new Vector2(10, 10);
+        private const int MoveStepPlayer = 5;
+
+        private Vector3[] _positionThangs = new Vector3[1]
+        {
+            new Vector3(Engine.Renderer.Camera.Width / 2 - SizeThang.X, Engine.Renderer.Camera.Height / 2 - SizeThang.Y, 1)
+        };
+
+        private static readonly Vector2 SizeThang = new Vector2(7, 7);
+        private const int MoveStepThang = 4;
 
         public override void Load()
         {
@@ -30,33 +42,56 @@ namespace Solution12
                 switch (key)
                 {
                     case KeyCode.A:
-                        modX -= MoveStep;
+                        modX -= MoveStepPlayer;
                         break;
                     case KeyCode.D:
-                        modX += MoveStep;
+                        modX += MoveStepPlayer;
                         break;
                     case KeyCode.W:
-                        modY -= MoveStep;
+                        modY -= MoveStepPlayer;
                         break;
                     case KeyCode.S:
-                        modY += MoveStep;
+                        modY += MoveStepPlayer;
                         break;
                 }
             }
-            
-            Console.WriteLine($"FT: {Engine.FrameTime.ToString(CultureInfo.InvariantCulture)}");
-            Console.WriteLine($"modX: {modX} | modY: {modY}");
 
-            _position.X = Math.Clamp(_position.X + modX, 0, Engine.Renderer.Camera.Width - _size.X);
-            _position.Y = Math.Clamp(_position.Y + modY, 0, Engine.Renderer.Camera.Height - _size.Y);
+            _positionPlayer.X = Math.Clamp(_positionPlayer.X + modX, 0, Engine.Renderer.Camera.Width - _sizePlayer.X);
+            _positionPlayer.Y = Math.Clamp(_positionPlayer.Y + modY, 0, Engine.Renderer.Camera.Height - _sizePlayer.Y);
+
+//            _positionThangs[0].X += _random.Next(-1, 2) * MoveStepThang;
+//            _positionThangs[0].Y += _random.Next(-1, 2) * MoveStepThang;
+
+            for (int i = 0; i < _positionThangs.Length; i++)
+            {
+                switch (i % 6)
+                {
+                    case 0:
+                        _positionThangs[i] = Vector3.Lerp(_positionPlayer, _positionThangs[i], 0.999f);
+                        break;
+                }
+
+//                _positionThangs[i] = Vector3.TransformNormal()
+            }
+
+//            foreach (var t in _positionThangs)
+//            {
+//                var thang = t;
+//                thang.X = Vector3.Lerp(thang._positionPlayer)
+//                thang.X += _random.Next(-1, 2) * MoveStepThang;
+//                thang.Y += _random.Next(-1, 2) * MoveStepThang;
+//            }
         }
 
         public override void Draw()
         {
 //            Console.WriteLine("SoScene - Draw");
-            
+
             Engine.Renderer.RenderOutline(Engine.Renderer.Camera.Position, Engine.Renderer.Camera.Size, Color.CornflowerBlue);
-            Engine.Renderer.Render(_position, _size, Color.Magenta);
+            Engine.Renderer.Render(_positionPlayer, _sizePlayer, Color.Magenta);
+
+            foreach (var thang in _positionThangs)
+                Engine.Renderer.Render(thang, SizeThang, Color.Pink);
         }
 
         public override void Unload()
