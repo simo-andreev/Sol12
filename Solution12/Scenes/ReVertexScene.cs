87fecda035e68f989e75419e885071922bb697fc
Simo Andreev <simeon.zlatanov.andreev@gmail.com>
2019-10-09 18:21:20 +0300

    feat: MISC - separate RenderVertices toys.

---
+++ Solution12/Scenes/ReVertexScene.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Numerics;
+using Adfectus.Common;
+using Adfectus.ImGuiNet;
+using Adfectus.Primitives;
+using Adfectus.Scenography;
+using ImGuiNET;
+
+namespace Solution12.Scenes
+{
+    public class ReVertexScene : Scene
+    {
+        private Random _rand = new Random();
+        private Vector3[,] mapMatrix = new Vector3[50, 20];
+
+        public override void Load()
+        {
+            /* do noting */
+        }
+
+        public override void Update()
+        {
+            /* do noting */
+        }
+
+        public override void Draw()
+        {
+            DrawGui();
+            DrawMatrix();
+        }
+
+        public override void Unload()
+        {
+            /* do noting */
+        }
+
+        private void DrawMatrix()
+        {
+            for (var i = 0; i < mapMatrix.GetLength(0) - 1; i++)
+            {
+                for (var j = 0; j < mapMatrix.GetLength(1) - 1; j++)
+                {
+                    var vertices = new[] {mapMatrix[i, j], mapMatrix[i + 1, j], mapMatrix[i + 1, j + 1], mapMatrix[i, j + 1]};
+                    var colors = new Color[4];
+                    for (var k = 0; k < vertices.Length; k++)
+                        colors[k] = new Color(20, (int) vertices[k].Z, 20);
+                    Engine.Renderer.RenderVertices(vertices, colors);
+                }
+            }
+
+            foreach (var vector3 in mapMatrix)
+            {
+                Engine.Renderer.RenderOutline(vector3, new Vector2(10f), Color.Black, 0.1f);
+            }
+        }
+
+        private void DrawGui()
+        {
+            ImGui.NewFrame();
+            ImGui.Begin("", ImGuiWindowFlags.NoResize);
+            if (ImGui.Button("reset"))
+                RegenMapMatrix();
+
+            Engine.Renderer.RenderGui();
+        }
+
+
+        private void RegenMapMatrix()
+        {
+            for (var i = 0; i < mapMatrix.GetLength(0); i++)
+            {
+                for (var j = 0; j < mapMatrix.GetLength(1); j++)
+                {
+                    mapMatrix[i, j] = new Vector3(i * 10, j * 10, _rand.Next(256));
+                }
+            }
+        }
+    }
+}
--- Solution12/Scenes/VertexScene.cs
+++ Solution12/Scenes/VertexScene.cs
@@ -12,21 +12,13 @@ namespace Solution12.Scenes
 {
     public class VeryScene : Scene
     {
-        private Random _rand = new Random();
+        private Atlas _atlasUbuntuMonoSmall;
+        private List<Color> _colors = new List<Color>();
+        private readonly Random _rand = new Random();
 
         private int _vertexCount = 1;
 
         private List<Vector3> _vertices = new List<Vector3>();
-        private List<Color> _colors = new List<Color>();
-        private Atlas _atlasUbuntuMonoSmall;
-
-        private Vector3[,] mapMatrix = new Vector3[50, 20];
-
-        public VeryScene()
-        {
-            RegenVertices();
-        }
-
 
         public override void Load()
         {
@@ -41,16 +33,13 @@ namespace Solution12.Scenes
         public override void Draw()
         {
             DrawGui();
-
-//            DrawVertices();
-            DrawMatrix();
-            
+            DrawVertices();
             LabelVertices();
         }
 
         private void LabelVertices()
         {
-            for (int i = 0; i < _vertices.Count; i++)
+            for (var i = 0; i < _vertices.Count; i++)
             {
                 var vertex = _vertices[i];
 
@@ -64,35 +53,12 @@ namespace Solution12.Scenes
             Engine.Renderer.RenderVertices(_vertices.ToArray(), _colors.ToArray());
         }
 
-        private void DrawMatrix()
-        {
-            for (var i = 0; i < mapMatrix.GetLength(0) - 1; i++)
-            {
-                for (var j = 0; j < mapMatrix.GetLength(1) - 1; j++)
-                {
-                    var vertices = new[] {mapMatrix[i, j], mapMatrix[i + 1, j], mapMatrix[i + 1, j + 1], mapMatrix[i, j + 1]};
-                    var colors = new Color[4];
-                    for (var k = 0; k < vertices.Length; k++)
-                        colors[k] = new Color(20, (int) vertices[k].Z, 20);
-                    Engine.Renderer.RenderVertices(vertices, colors);
-                }
-            }
-
-            foreach (var vector3 in mapMatrix)
-            {
-                Engine.Renderer.RenderOutline(vector3, new Vector2(10f), Color.Black, 0.1f);
-            }
-        }
-
         private void DrawGui()
         {
             ImGui.NewFrame();
             ImGui.Begin("", ImGuiWindowFlags.NoResize);
             if (ImGui.Button("reset"))
-            {
-//                RegenVertices();
-                RegenMapMatrix();
-            }
+                RegenVertices();
 
             ImGui.InputInt("Vertex Count:", ref _vertexCount);
 
@@ -121,17 +87,6 @@ namespace Solution12.Scenes
 
                 _vertices.Add(vertex);
                 _colors.Add(new Color((int) vertex.Z, 100, 100));
-            }
-        }
-
-        private void RegenMapMatrix()
-        {
-            for (var i = 0; i < mapMatrix.GetLength(0); i++)
-            {
-                for (var j = 0; j < mapMatrix.GetLength(1); j++)
-                {
-                    mapMatrix[i, j] = new Vector3(i * 10, j * 10, _rand.Next(256));
-                }
             }
         }
     }
