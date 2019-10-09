363e4ad246c0b80cba1d21ef4b87eaed2783e002
Simo Andreev <simeon.zlatanov.andreev@gmail.com>
2019-10-09 18:25:29 +0300

    idk, some BS probs.

--- Solution12/Scenes/ReVertexScene.cs
+++ Solution12/Scenes/ReVertexScene.cs
@@ -8,10 +8,13 @@ using ImGuiNET;
 
 namespace Solution12.Scenes
 {
+    /// <summary>
+    /// WARNING: Carcinogenic material ahead.
+    /// </summary>
     public class ReVertexScene : Scene
     {
-        private Random _rand = new Random();
-        private Vector3[,] mapMatrix = new Vector3[50, 20];
+        private readonly Random _rand = new Random();
+        private Vector3[,] _mapMatrix = new Vector3[50, 20];
 
         public override void Load()
         {
@@ -36,11 +39,11 @@ namespace Solution12.Scenes
 
         private void DrawMatrix()
         {
-            for (var i = 0; i < mapMatrix.GetLength(0) - 1; i++)
+            for (var i = 0; i < _mapMatrix.GetLength(0) - 1; i++)
             {
-                for (var j = 0; j < mapMatrix.GetLength(1) - 1; j++)
+                for (var j = 0; j < _mapMatrix.GetLength(1) - 1; j++)
                 {
-                    var vertices = new[] {mapMatrix[i, j], mapMatrix[i + 1, j], mapMatrix[i + 1, j + 1], mapMatrix[i, j + 1]};
+                    var vertices = new[] {_mapMatrix[i, j], _mapMatrix[i + 1, j], _mapMatrix[i + 1, j + 1], _mapMatrix[i, j + 1]};
                     var colors = new Color[4];
                     for (var k = 0; k < vertices.Length; k++)
                         colors[k] = new Color(20, (int) vertices[k].Z, 20);
@@ -48,7 +51,7 @@ namespace Solution12.Scenes
                 }
             }
 
-            foreach (var vector3 in mapMatrix)
+            foreach (var vector3 in _mapMatrix)
             {
                 Engine.Renderer.RenderOutline(vector3, new Vector2(10f), Color.Black, 0.1f);
             }
@@ -67,11 +70,11 @@ namespace Solution12.Scenes
 
         private void RegenMapMatrix()
         {
-            for (var i = 0; i < mapMatrix.GetLength(0); i++)
+            for (var i = 0; i < _mapMatrix.GetLength(0); i++)
             {
-                for (var j = 0; j < mapMatrix.GetLength(1); j++)
+                for (var j = 0; j < _mapMatrix.GetLength(1); j++)
                 {
-                    mapMatrix[i, j] = new Vector3(i * 10, j * 10, _rand.Next(256));
+                    _mapMatrix[i, j] = new Vector3(i * 10, j * 10, _rand.Next(256));
                 }
             }
         }
