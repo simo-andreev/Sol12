d31133e947e16443981c25a298d48bdede8b3050
Simo Andreev <simeon.zlatanov.andreev@gmail.com>
2019-10-16 02:24:11 +0300

    fix: MISC - reintroduce the Scene Selection SceneScene Scene.

--- Solution12/Scenes/SceneScene.cs
+++ Solution12/Scenes/SceneScene.cs
@@ -1,51 +1,52 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Reflection;
-//using Adfectus.Common;
-//using Adfectus.ImGuiNet;
-//using Adfectus.Scenography;
-//using ImGuiNET;
-//
-//namespace Solution12.Scenes
-//{
-//    public class SceneScene : Scene
-//    {
-//        private List<Type> _scenes;
-//
-//        public override void Load()
-//        {
-//            // Get all : Scene classes in ~package~ namespace.
-//            _scenes = Assembly.GetExecutingAssembly().GetTypes()
-//                .Where(t => t.Namespace == "Solution12.Scenes")
-//                .Where(type => typeof(Scene).IsAssignableFrom(type))
-//                .ToList();
-//        }
-//
-//        public override void Update()
-//        {
-//            /* do noting */
-//        }
-//
-//        public override void Draw()
-//        {
-//            ImGui.NewFrame();
-//
-//            foreach (var scene in _scenes)
-//            {
-//                if (ImGui.Button(scene.Name))
-//                {
-//                    Engine.SceneManager.SetScene((Scene) Activator.CreateInstance(scene));
-//                    break;
-//                }
-//            }
-//
-//            Engine.Renderer.RenderGui();
-//        }
-//
-//        public override void Unload()
-//        {
-//            /* do noting */
-//        }
-//    }
-//}
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Emotion.Common;
+using Emotion.Graphics;
+using Emotion.Plugins.ImGuiNet;
+using Emotion.Scenography;
+using ImGuiNET;
+
+namespace Solution12.Scenes
+{
+    public class SceneScene : IScene
+    {
+        private List<Type> _scenes;
+        
+        public void Load()
+        {
+            // Get all : Scene classes in ~package~ namespace.
+            _scenes = Assembly.GetExecutingAssembly().GetTypes()
+                .Where(t => t.Namespace == "Solution12.Scenes")
+                .Where(type => typeof(IScene).IsAssignableFrom(type))
+                .ToList();
+        }
+
+        public void Update()
+        {
+            /* do noting */
+        }
+
+        public void Draw(RenderComposer composer)
+        {
+            ImGui.NewFrame();
+
+            foreach (var scene in _scenes)
+            {
+                if (ImGui.Button(scene.Name))
+                {
+                    Engine.SceneManager.SetScene((IScene) Activator.CreateInstance(scene));
+                    break;
+                }
+            }
+            
+            composer.RenderUI();
+        }
+
+        public void Unload()
+        {
+            /* do noting */
+        }
+    }
+}
