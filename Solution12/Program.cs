87745a2a250346170217bf03ed990e3d08cc77eb
Simo Andreev <simeon.zlatanov.andreev@gmail.com>
2020-01-20 23:28:44 +0200

    feat: Camera - push @Vlad cam code.

--- Solution12/Program.cs
+++ Solution12/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿#region Using
+
+using System;
 using System.Numerics;
 using Emotion.Common;
 using Emotion.Graphics.Camera;
@@ -7,11 +9,13 @@ using Emotion.Platform.Input;
 using Emotion.Plugins.ImGuiNet;
 using Solution12.Scenes;
 
+#endregion
+
 namespace Solution12
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
 
@@ -34,44 +38,72 @@ namespace Solution12
             Engine.Run();
         }
     }
-    
-    
-    class SolCam : PixelArtCamera
+}
+
+internal class SolCam : CameraBase
+{
+    public float Speed { get; set; } = 0.35f;
+
+    public SolCam(Vector3 position, float zoom = 1) : base(position, zoom)
     {
-        public SolCam(Vector3 position, float zoom = 1) : base(position, zoom)
-        {
-        }
+    }
 
-        public override void Update()
-        {
-            base.Update();
+    /// <inheritdoc />
+    public override void RecreateMatrix()
+    {
+        Vector2 targetSize = Engine.Configuration.RenderSize;
+        Vector2 currentSize = Engine.Renderer.DrawBuffer.Size;
 
-            Vector3 cameraMoveDirection = Vector3.Zero;
+        // Transform the position from the center position to the offset position.
+        Vector3 posOffset = new Vector3(X, Y, 0) - new Vector3(targetSize, 0) / 2;
 
-            // note any-and-all 'WASD' move input
-            if (Engine.InputManager.IsKeyHeld(Key.W)) cameraMoveDirection.Y -= 1;
-            if (Engine.InputManager.IsKeyHeld(Key.A)) cameraMoveDirection.X -= 1;
-            if (Engine.InputManager.IsKeyHeld(Key.S)) cameraMoveDirection.Y += 1;
-            if (Engine.InputManager.IsKeyHeld(Key.D)) cameraMoveDirection.X += 1;
+        // Get the scale relative to the zoom.
+        float scale = Engine.Renderer.Scale * Zoom;
 
-            // If mouse scroll-ed, note Zoom amount and direction
-            if (Engine.InputManager.GetMouseScrollRelative() == -1) Zoom += 0.035f * Engine.DeltaTime;
-            if (Engine.InputManager.GetMouseScrollRelative() == 1) Zoom -= 0.035f * Engine.DeltaTime;
+        #region integer_scale
 
-            // Clamp Camera Zoom
-            if (Zoom > 6) Zoom = 6;
-            if (Zoom < 0.5) Zoom = 0.5f;
+        // Find the camera margin and scale from the center.
+        // As the current size expands more of the world will come into view until the integer scale changes at which point everything will be resized.
+        // This allows for pixel art to scale integerly in FullScale mode.
+        Vector2 margin = (currentSize - targetSize) / 2;
+        Vector3 pos = posOffset - new Vector3((int) margin.X, (int) margin.Y, 0);
+        ViewMatrixUnscaled = Matrix4x4.CreateLookAt(pos, pos + new Vector3(0, 0, -1), new Vector3(0.0f, 1.0f, 0.0f));
+        ViewMatrix = Matrix4x4.CreateScale(new Vector3(scale, scale, 1), new Vector3(X, Y, 0)) * ViewMatrixUnscaled;
 
-            var speed = 0.35f;
-            // If fast-move key down -> quadruple speed coefficient 
-            if (Engine.InputManager.IsKeyHeld(Key.LeftControl)) speed *= 4;
+        #endregion
+    }
 
-            // Vect-multiply the recorded camera movement input by DeltaT'd speed coefficient in all axes.
-            cameraMoveDirection *= new Vector3(speed * Engine.DeltaTime, speed * Engine.DeltaTime, speed * Engine.DeltaTime);
+    public override void Update()
+    {
+        Vector3 cameraMoveDirection = Vector3.Zero;
 
-            // Finally apply the movement Vector to the camera and RecreateMatrix
-            Engine.Renderer.Camera.Position += cameraMoveDirection;
-            RecreateMatrix();
-        }
+        // note any-and-all 'WASD' move input
+        if (Engine.InputManager.IsKeyHeld(Key.W)) cameraMoveDirection.Y -= 1;
+        if (Engine.InputManager.IsKeyHeld(Key.A)) cameraMoveDirection.X -= 1;
+        if (Engine.InputManager.IsKeyHeld(Key.S)) cameraMoveDirection.Y += 1;
+        if (Engine.InputManager.IsKeyHeld(Key.D)) cameraMoveDirection.X += 1;
+
+        // If mouse scroll-ed, note Zoom amount and direction
+        if (Engine.InputManager.GetMouseScrollRelative() == -1) Zoom += 0.035f * Engine.DeltaTime;
+        if (Engine.InputManager.GetMouseScrollRelative() == 1) Zoom -= 0.035f * Engine.DeltaTime;
+
+        // Clamp Camera Zoom
+        if (Zoom > 6) Zoom = 6;
+        if (Zoom < 0.5) Zoom = 0.5f;
+
+        // If fast-move key down -> quadruple speed coefficient 
+        float speed = Speed;
+        if (Engine.InputManager.IsKeyHeld(Key.LeftControl)) speed *= 4;
+        speed *= Engine.DeltaTime;
+
+        // Since the camera is isometric its X and Y axes are rotated - however we want the movement to be relative to the screen 
+        // based on the perspective. To do this we need to find the "proper" up and right directions and move along them.
+        cameraMoveDirection *= new Vector3(speed, speed, speed);
+
+        // Finally apply the movement Vector to the camera.
+        Engine.Renderer.Camera.Position += cameraMoveDirection;
+
+        // todo: Currently the matrix must be manually recreated as zooming doesn't trigger a recreation.
+        RecreateMatrix();
     }
 }
